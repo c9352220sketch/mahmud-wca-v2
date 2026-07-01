d6a49a1b06053c7036db32721634e3527013aa2c
c9352220sketch <c9352220@gmail.com>
2026-07-01 06:47:51 +0600

    feat: add src/api/sendTypingIndicator.js

---
+++ src/api/sendTypingIndicator.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const { idToJID } = require("../utils");
+
+/**
+ * sendTypingIndicator(threadID, isTyping, callback?)
+ * FCA-compatible typing indicator.
+ */
+module.exports = (sock) =>
+  async function sendTypingIndicator(threadID, isTyping = true, callback) {
+    const jid = idToJID(threadID);
+    try {
+      await sock.sendPresenceUpdate(isTyping ? "composing" : "paused", jid);
+      if (typeof callback === "function") callback(null);
+    } catch (err) {
+      if (typeof callback === "function") callback(err);
+    }
+  };
